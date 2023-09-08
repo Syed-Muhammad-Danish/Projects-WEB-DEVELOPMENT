0573e90f4519e656b415e740fa4af9401718bde8
danish80 <syedm.82004@gmail.com>
2023-09-09 00:15:58 +0500

    Projects Commit

---
+++ ecommerce/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer" style="background-color: darkgray">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2020 Brought to You By Danish</strong>
+    </div>
+</footer>
